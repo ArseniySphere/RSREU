24da77ec80a7d1be9483867fd707f566cbdfde36
Guskov A.S. <arceniyguskov@gmail.com>
2025-03-12 22:12:54 +0300

    alg_lab_2

--- 4 semester/Alg/lab_2.py
+++ 4 semester/Alg/lab_2.py
@@ -1,9 +1,9 @@
-import math
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 import matplotlib.pyplot as plt
 import numpy as np
 from PyQt5.QtWidgets import QMessageBox
+import decimal
 
 
 class Ui_MainWindow(object):
@@ -102,6 +102,7 @@ class Ui_MainWindow(object):
         # Code
         self.eps = 1e-4
         self.do = True
+        self.max_iter = 100
         self.pushButton.clicked.connect(self.display_gr)
         self.pushButton.clicked.connect(self.show_answers)
 
@@ -109,8 +110,8 @@ class Ui_MainWindow(object):
         self.do = True
         self.k = self.spinBox_k.value()
         self.m = self.spinBox_m.value()
-        if self.m and self.k:
-            x = np.linspace(-10, 10, 100)
+        if 0 < self.m < 4 and 0 < self.k < 4:
+            x = np.linspace(-5, 5, 1000)
             y = self.m * x ** 2 - self.k * 2.71828 ** x
             plt.plot(x, y)
             plt.title('График функции')
@@ -131,6 +132,27 @@ class Ui_MainWindow(object):
     def func(self, x):
         return self.m * x ** 2 - self.k * 2.71828 ** x
 
+    def der1_func(self, x):
+        return self.m * 2 * x - self.k * 2.71828 ** x
+
+    def der2_func(self, x):
+        return self.m * 2 - self.k * 2.71828 ** x
+
+    def f_iter(self, x):
+        return self.k * 2.71828 ** x - self.m * x ** 2 - 3 * x
+
+
+    def find_interval(self):
+        a = -10
+        right = 10
+        b = -9
+        while b != right:
+            if self.func(a) * self.func(b) < 0:
+                return [a, b]
+            a += 0.1
+            b += 0.1
+
+
     def m_dih(self, a, b):
         while(abs(b - a) > self.eps):
             x = (a + b) / 2
@@ -140,16 +162,126 @@ class Ui_MainWindow(object):
                 a = x
             else:
                 b = x
-        return x
+        accur = round(abs(b-a), 6)
+        return x, accur
 
-    def m_hor(self):
+    def m_hor(self, a, b):
+        left = a
+        right = b
+        z = self.func(a)
+        t = self.func(b)
+        x = left
+        while True:
+            last = x
+            x = left - ((right - left) / (t - z)) * z
+            y = self.func(x)
+            if y * z < 0:
+                right = x
+                t = y
+            else:
+                left = x
+                z = y
+            if abs(last - x) < self.eps:
+                accur = round(abs(last - x), 6)
+                return x, accur
 
+    def m_kas(self,a , b):
+        left = a
+        right = b
+        if self.func(left) * self.der2_func(left) > 0:
+            x = left
+        elif self.func(right) * self.der2_func(right):
+            x = right
+        else:
+            self.err()
+            exit(1)
+        while True:
+            h = self.func(x) / self.der1_func(x)
+            x = x - h
+            if abs(h) < self.eps:
+                accur = round(abs(h), 6)
+                return x, accur
+
+    def m_m_kas(self, a, b):
+        left = a
+        right = b
+        previous = float('inf')
+
+        if self.func(left) * self.der2_func(left) > 0:
+            x = left
+        else:
+            x = right
+
+        d = self.der1_func(x)
+
+        while abs(x - previous) > self.eps:
+            previous = x
+            x -= self.func(x) / d
+        accur = round(abs(x - previous), 6)
+        return x, accur
+
+    def m_comb(self, a, b):
+        left = a
+        right = b
+        prev = 0
+
+        if self.func(left) * self.der2_func(left) > 0:
+            x = left
+            y = right
+        else:
+            x = right
+            y = left
+
+        while abs(x - y) > self.eps:
+            mx = x - self.func(x) / self.der1_func(x)
+            my = y - self.func(y) * (x - y) / (self.func(x) - self.func(y))
+
+            prev = x
+            x = mx
+            y = my
+        ans = float((x + y) / 2)
+        accur = round(abs(x - prev), 6)
+        return ans, accur
+
+    def m_iter(self, a, b):
+        left = a
+        right = b
+        prev = 0
+        x = (left + right) / 2
+        m = max([self.func(left), self.func(right), self.func(x)])
+        for _ in range(self.max_iter):
+            if abs(x - prev) < self.eps:
+                accur = round(abs(x - prev), 6)
+                return x, accur
+
+            mx = x + self.func(x) / m
+            m = max(m, self.func(mx))
+
+            prev = x
+            x = mx
 
     def show_answers(self):
         if self.do:
             _translate = QtCore.QCoreApplication.translate
-            ans = self.m_dih(-3, 0)
-            self.label_2.setText(_translate("MainWindow", f"Дихотомии: {ans}"))
+            left, right = self.find_interval()
+            ans, accur = self.m_dih(left, right)
+            self.label_2.setText(_translate("MainWindow", f"Дихотомии: {ans}, погр. {accur}"))
+
+            ans, accur = self.m_hor(left, right)
+            self.label_3.setText(_translate("MainWindow", f" хорд: {ans}, погр. {accur}"))
+
+            ans, accur = self.m_kas(left, right)
+            self.label_4.setText(_translate("MainWindow", f"касательных: {ans}, погр. {accur}"))
+
+            ans, accur = self.m_m_kas(left, right)
+            self.label_5.setText(_translate("MainWindow", f"м. касательных: {ans}, погр. {accur}"))
+
+            ans, accur = self.m_comb(left, right)
+            self.label_6.setText(_translate("MainWindow", f"комбинированным: {ans}, погр. {accur}"))
+
+            ans, accur = self.m_iter(left, right)
+            self.label_7.setText(_translate("MainWindow", f"итерационным: {ans}, погр. {accur}"))
+
 
 
 
